d44f8664764930d5eaad926ad6d28db372b3c2c7
Ahmet Ince <ahmetince771@gmail.com>
2021-02-10 00:39:10 +0000

    added other project modals

--- js/show-modals.js
+++ js/show-modals.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
         e.preventDefault();
         $('#suleymaniye-aid-project-modal').modal('show').find('.modal-content').load($(this).attr('href'));
     });
+
+    $('#suleymaniye-org-project').on('click', function (e) {
+        e.preventDefault();
+        $('#suleymaniye-org-project-modal').modal('show').find('.modal-content').load($(this).attr('href'));
+    });
+
+    $('#interview-revision-project').on('click', function (e) {
+        e.preventDefault();
+        $('#interview-revision-project-modal').modal('show').find('.modal-content').load($(this).attr('href'));
+    });
+
+    $('#password-generator-project').on('click', function (e) {
+        e.preventDefault();
+        $('#password-generator-project-modal').modal('show').find('.modal-content').load($(this).attr('href'));
+    });
+
+    $('#nextbook-project').on('click', function (e) {
+        e.preventDefault();
+        $('#nextbook-project-modal').modal('show').find('.modal-content').load($(this).attr('href'));
+    });
 });
