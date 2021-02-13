bab6bca3003d70934d7f3f548d564c5ed2c38ad4
Ahmet Ince <ahmetince771@gmail.com>
2021-02-13 13:04:06 +0000

    able to add active-btn onclick

--- js/filter-projects.js
+++ js/filter-projects.js
@@ -1,24 +1,14 @@
-// $(document).ready(function () {
-//     // https://bootsnipp.com/snippets/r1Z7d
-//     $(".filter-button").click(function () {
-//         var value = $(this).attr('data-filter');
-
-//         if (value == "all") {
-//             $('.filter').show('1000');
-//         } else {
-//             $(".filter").not('.' + value).hide('3000');
-//             $('.filter').filter('.' + value).show('3000');
-//         }
-//     });
-// });
-
 // https://isotope.metafizzy.co/filtering.html
 // https://jsfiddle.net/shirandror/rvnrk2kc/
 $(document).ready(function () {
     $grid = $('.grid');
     $grid.isotope();
 
-    $('#project-filter-all-button,#project-filter-web-button, #project-filter-mobile-button').click(function () {
+    $('#project-filter-all-button, #project-filter-web-button, #project-filter-mobile-button').click(function () {
+        // remove active-btn class from everywhere 
+        $('.active-btn').removeClass('active-btn');
+        // add active-btn class on the element
+        $(this).addClass('active-btn');
 
         var id = $(this).attr('id');
         var className = id.replace("-button", "");
